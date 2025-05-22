2a5556d4a6817023e15ceb925fab48440572160f
dcabrerap97 <dieg.cabrerap@duocuc.cl>
2025-05-22 18:42:50 -0400

    Se crea clase usuario

---
+++ perfulandia/src/main/java/com/pixelpear/perfulandia/modelo/usuario.java
@@ -0,0 +1,20 @@
+package com.pixelpear.perfulandia.modelo;
+import jakarta.persistence.*;
+import lombok.Data.*;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+
+public class usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    private String nombre;
+    private String correo;
+    private String rol;
+    private Boolean activo;
+
+}
