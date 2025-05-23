9ac291a77f807ee0c888598376273d57f758e430
dcabrerap97 <dieg.cabrerap@duocuc.cl>
2025-05-23 01:04:08 -0400

    correccion de errores en el repositorio

--- perfulandia/src/main/java/com/pixelpear/perfulandia/repositorio/usuario_repositorio.java
+++ perfulandia/src/main/java/com/pixelpear/perfulandia/repositorio/usuario_repositorio.java
@@ -1,10 +1,11 @@
 package com.pixelpear.perfulandia.repositorio;
 
 import com.pixelpear.perfulandia.modelo.usuario;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.jpa.repositorio.Jparepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface usuario_repositorio extends JpaRepository<usuario, Long>{
 
 
