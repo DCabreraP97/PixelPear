b779a3804048dfa6ceaf42199ea2a418cae383df
dcabrerap97 <dieg.cabrerap@duocuc.cl>
2025-05-22 23:17:53 -0400

    avance 1.0

---
+++ perfulandia/src/main/java/com/pixelpear/perfulandia/repositorio/usuario_repositorio.java
@@ -0,0 +1,12 @@
+package com.pixelpear.perfulandia.repositorio;
+
+import com.pixelpear.perfulandia.modelo.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.jpa.repositorio.Jparepository;
+
+public interface usuario_repositorio extends JpaRepository{
+
+    
+}   
+
